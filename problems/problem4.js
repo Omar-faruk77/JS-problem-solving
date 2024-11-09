95207c807d2a54961109385040c11303c02fea26
Omar-faruk77 <gameghor667@gmail.com>
2024-11-09 19:48:48 +0600

    JavaScript Problem Solved

---
+++ problems/problem4.js
@@ -0,0 +1,7 @@
+let length = 16; // data type: number
+let lastName = "Johnson"; // data type: string
+
+const x = {
+  firstName: "John",
+  lastName: "Doe"
+}; // data type: object
---
+++ problems/problem9.js
@@ -0,0 +1,15 @@
+let randomNumber = Math.random();
+
+console.log(randomNumber)
+
+
+let largestNumber = Math.max(10, 20);
+
+console.log(largestNumber)
+
+
+let squareRoot = Math.sqrt(9);
+
+console.log(squareRoot)
+
+
